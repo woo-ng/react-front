2de356a0d05391b516570d0d0750890357c0cd14
oceanfromthewave <oceanfromthewave@gmail.com>
2023-02-27 15:11:55 +0900

    feat: 로그인 화면 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createBrowserHistory } from 'history';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Login from './component/layout/Login';
 import Main from './component/layout/Main';
 
 const history = createBrowserHistory();
---
+++ src/component/layout/Login.js
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import './Login.scss';
+import { ReactComponent as Logo } from '../../logo.svg';
+
+function Login() {
+  const [memberId, setMemberId] = useState('');
+  const [memberPw, setMemberPw] = useState('');
+  const [loginResult, setLoginResult] = useState(null);
+
+  function handleLogin() {
+    fetch('/member/login.do', {
+      method: 'POST',
+      body: JSON.stringify({ memberId, memberPw }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          // handle successful login
+        } else {
+          setLoginResult('사용자 ID 또는 비밀번호를 잘못 입력하셨습니다.');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  return (
+    <>
+      <a href="/">
+        <Logo className="logo_svg" height="43" />
+      </a>
+      <div className="wrapper">
+        <div className="wrap">
+          <form>
+            <div className="logo_wrap">
+              <span>Book Mall</span>
+            </div>
+            <div className="login_wrap">
+              <div className="id_wrap">
+                <div className="id_input_box">
+                  <input
+                    className="id_input"
+                    type="text"
+                    name="memberId"
+                    value={memberId}
+                    onChange={(event) => setMemberId(event.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="pw_wrap">
+                <div className="pw_input_box">
+                  <input
+                    className="pw_input"
+                    type="password"
+                    name="memberPw"
+                    value={memberPw}
+                    onChange={(event) => setMemberPw(event.target.value)}
+                  />
+                </div>
+              </div>
+              {loginResult && <div className="login_warn">{loginResult}</div>}
+              <div className="login_button_wrap">
+                <button
+                  type="button"
+                  className="login_button"
+                  onClick={handleLogin}
+                >
+                  로그인
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
